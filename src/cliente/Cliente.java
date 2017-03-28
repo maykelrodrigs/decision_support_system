0808822d4b28eacb4b27eaa7234a5a2bfb5c4cea
Maykel Rodrigues <maykelrodrigues@live.com>
2017-03-28 11:46:44 -0300

    Criado a interface

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -5,8 +5,11 @@
  */
 package cliente;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 import javax.swing.JOptionPane;
 
@@ -16,20 +19,50 @@ import javax.swing.JOptionPane;
  */
 public class Cliente {
     
+    private int porta = 12345;
+
+    public Cliente() {
+          
+    }
     
-   
-    void Enviar() {
+    ArrayList ReceberDados() {
+        
+        ArrayList dados = new ArrayList();
         
         try {
-            Socket cliente = new Socket("paulo", 12345);
+            
+            Socket cliente = new Socket(InetAddress.getLocalHost().getHostName(), porta);
             ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-            Date data_atual = (Date)entrada.readObject();
-            JOptionPane.showMessageDialog(null,"Data recebida do servidor:" + data_atual.toString());
+            dados = (ArrayList) entrada.readObject();
             entrada.close();
+            
+          }
+          catch(IOException | ClassNotFoundException e) {
+            System.out.println("Erro servidor: " + e.getMessage());
+          }
+        
+        return dados;
+    }
+
+    public static void main(String[] args) {
+           
+        try {
+            
+            Socket cliente = new Socket(InetAddress.getLocalHost().getHostName(), 12345);
+            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+            
+            ArrayList <String> sintomas = new ArrayList();
+            sintomas = (ArrayList<String>) entrada.readObject();
+            
+            //Date data_atual = (Date)entrada.readObject();
+            JOptionPane.showMessageDialog(null,"Data recebida do servidor:" + sintomas.get(1));
+            entrada.close();
+            
             System.out.println("Conexão encerrada");
+            
           }
           catch(Exception e) {
-            System.out.println("Erro: " + e.getMessage());
+            System.out.println("Erro servidor: " + e.getMessage());
           }
 
     }
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -8,6 +8,7 @@ package servidor;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -15,11 +16,74 @@ import java.util.Date;
  * @author mrodrigues
  */
 public class Servidor {
+    
+    private ArrayList <String> paciente;
+    private ArrayList <String> sintomas;
+    private ArrayList <String> doenca;
+    private ArrayList <String> medico;
+    
+    private int matriz[][]; // 20 14
+    private int atendimentos;
+    
+    public Servidor() {
+        
+        this.matriz = new int[20][14];
+        this.paciente = new ArrayList<>();
+        this.sintomas = new ArrayList<>();
+        this.doenca = new ArrayList<>();
+        this.medico = new ArrayList<>();
+        this.atendimentos = 0;
+        
+        sintomas.add("Febre");
+        sintomas.add("Manchas Cutaneas");
+        sintomas.add("Cansaço");
+        sintomas.add("Olhos irritados");
+        sintomas.add("Dores nas articulaçoes");
+        sintomas.add("Vomito");
+        sintomas.add("Diarreia");
+        sintomas.add("Tosse");
+        sintomas.add("Dor de cabeça");
+        sintomas.add("Dor de garganta");
+        
+        doenca.add("Resfriado");
+        doenca.add("Gripe");
+        doenca.add("Dengue");
+        doenca.add("Alergia");
+        doenca.add("Virose");
+        
+    }
 
+    public ArrayList<String> getPaciente() {
+        return paciente;
+    }
+
+    public ArrayList<String> getSintomas() {
+        return sintomas;
+    }
+
+    public ArrayList<String> getDoenca() {
+        return doenca;
+    }
+
+    public ArrayList<String> getMedico() {
+        return medico;
+    }
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+    public int getAtendimentos() {
+        return atendimentos;
+    }
+    
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Servidor s = new Servidor();
         
         try {
             
@@ -27,22 +91,22 @@ public class Servidor {
             System.out.println("Servidor ouvindo a porta 12345");
             
             while(true) {
-                
+              
               Socket cliente = servidor.accept();
+              
               System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
               ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+              
               saida.flush();
-              saida.writeObject(new Date());
+              saida.writeObject(s.getSintomas());
               saida.close();
               cliente.close();
               
             }  
         }   
-    catch(Exception e) {
-       System.out.println("Erro: " + e.getMessage());
-    }
-    finally {
-        } 
+        catch(Exception e) {
+           System.out.println("Erro: " + e.getMessage());
+        }
         
     }
     
